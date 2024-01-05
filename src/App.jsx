0c74daaee7145a6b1af9452a63a2d16b78e1f8bc
ne5oo <106486790+neo5oo@users.noreply.github.com>
2024-01-05 13:19:30 +0300

    todo react+redux

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { TodoList } from "./components/TodoList";
+import { InputField } from "./components/InputField";
+import "./App.css";
+import { addTodo } from "./store/todoSlice";
+
+function App() {
+  const [text, setText] = useState("");
+  const dispatch = useDispatch();
+
+  const addTask = () => {
+    dispatch(addTodo({ text }));
+    setText("");
+  };
+
+  return (
+    <div className="App">
+      <InputField text={text} handleInput={setText} handleSubmit={addTask} />
+
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
